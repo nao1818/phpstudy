d59c64a378267571f723c4f789a1a620d47e18d7
nao-18 <kyahyo17@yahoo.co.jp>
2018-07-12 08:28:08 +0900

    progate_php1 finish

---
+++ progate_php/PHP_LESSON1/PHP7/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // $ageという変数に自分の年齢を代入してください
+    $age = 30;
+
+    if ($age >= 30) {
+      echo "あなたは30歳以上です。";
+    } else {
+      echo "あなたは30歳未満です。";
+    }
+
+
+  ?>
+
+</body>
+</html>
